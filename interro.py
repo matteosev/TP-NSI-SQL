313aaf32d38df6b3c60de86b44d3ce5bb230ee85
matteosev <54771696+matteosev@users.noreply.github.com>
2021-01-30 20:53:08 +0100

    Add files via upload

--- interro.py
+++ interro.py
@@ -3,39 +3,40 @@ import tkinter as tk
 from code import *
 
 # on redéfinit la fonction pour bind chaque cellule du tableau
-def PrintTable(frame, colNames, content):
+def PrintTable(frame, dbConnection, tableName):
     """
     Affiche une table dans une frame tkinter.
 
     Paramètres:
         frame (tkinter.Frame):
             la frame dans laquelle afficher la table
-        colNames (list):
-            une liste contenant les noms des colonnes
-        content (list):
-            une liste (lignes) de listes (colonnes) contenant les données de la table
+        dbConnection (sqlite3.Connection):
+            la connexion vers la base de données contenant la table à afficher
+        tableName (str):
+            le nom de la table à afficher
 
     Valeur de retour:
         None
     """
+    colNames = GetColumnNames(dbConnection, tableName)
+    content = GetContent(dbConnection, tableName)
+    
     for widget in frame.winfo_children():
         widget.destroy()
 
     for col in range(len(colNames)):
         tk.Label(frame, text = colNames[col]).grid(row = 0, column = col)
 
+    entryVariables = []
     for row in range(len(content)):
+        entryVariables.append([])   # nouvelle ligne
         for col in range(len(content[row])):
-            label = tk.Label(frame, text = content[row][col])
-            label.bind('<Button-1>', lambda click: AskModif(click))
-            label.grid(row = row + 1, column = col)
+            entryVariables[row].append(tk.StringVar())
+            entryVariables[row][col].set(content[row][col])
+            entry = tk.Entry(frame, textvariable = entryVariables[row][col])
+            entry.grid(row = row + 1, column = col)
 
-
-def AskModif(click):
-    global tableau
-    print(click.x, click.y)
-    print(tableau.grid_size())
-    #print(tableau.grid_location(click.x, click.y))
+# TODO: faire un bouton "enregistrer" qui ne marche que quand un élément a été changé
 
 if __name__ == "__main__":
     dbConnection = sqlite3.connect("db.db")
@@ -43,8 +44,8 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("Modification d'une base de données")
     root.rowconfigure(0, weight=1)
-    root.columnconfigure(0, weight=2)
-    root.columnconfigure(1, weight=2)
+    root.columnconfigure(0, weight=1)
+    root.columnconfigure(1, weight=1)
 
     tableau = tk.Frame(root, relief="ridge", bd=1)
     interface = tk.Frame(root)
@@ -53,7 +54,7 @@ if __name__ == "__main__":
     
     tables = GetTableNames(dbConnection)
     choix = tk.StringVar()
-    choix.trace("w", lambda x, y, z: PrintTable(tableau, GetColumnNames(dbConnection, choix.get()), GetContent(dbConnection, choix.get())))
+    choix.trace("w", lambda x, y, z: PrintTable(tableau, dbConnection, choix.get()))
     choix.set(tables[0])
     menu = tk.OptionMenu(interface, choix, *tables)
     menu.grid(row=0, column=0)
